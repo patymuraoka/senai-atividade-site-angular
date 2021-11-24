e1f67b3dbdfc65e7e32ffd28770588586593350b
Patricia Muraoka <patymuraoka@gmail.com>
2021-11-23 23:15:09 -0300

    S3 - Encontro Presencial 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './pagina/contato/contato.component';
+import { HomeComponent } from './pagina/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: HomeComponent, pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'contato', component: ContatoComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './pagina/header/header.component';
+import { MenuComponent } from './pagina/menu/menu.component';
+import { HomeComponent } from './pagina/home/home.component';
+import { ContatoComponent } from './pagina/contato/contato.component';
+import { FooterComponent } from './pagina/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    HomeComponent,
+    ContatoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
